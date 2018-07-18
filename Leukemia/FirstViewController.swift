5298fafc23ffb642ed59584169b880e5f4022f0f
samtucker <sam.tucker1@icloud.com>
2018-07-18 11:43:02 +0100

    Initial Commit

---
+++ Leukemia/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Leukemia
+//
+//  Created by Sam Tucker on 18/07/2018.
+//  Copyright © 2018 Sam Tucker. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
